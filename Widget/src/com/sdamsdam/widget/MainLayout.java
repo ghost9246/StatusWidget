466071fcf2c4550afca91f83a2223597bc64dbc9
ruucm <bumbermin@naver.com>
2014-04-30 12:59:43 +0900

    d

---
+++ Widget/src/com/sdamsdam/widget/MainLayout.java
@@ -0,0 +1,20 @@
+package com.sdamsdam.widget;
+
+import android.app.*;
+import android.os.*;
+
+public class MainLayout extends Activity {
+	
+	
+	
+	
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainlayout);
+	}
+
+	
+}
