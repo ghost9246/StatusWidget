337d7caf39a2deb0b8c331f64bc73e20c4590929
ruucm <bumbermin@naver.com>
2014-05-01 23:42:50 +0900

    애니메이션으로 초기화

--- Widget/gen/com/sdamsdam/widget/R.java
+++ Widget/gen/com/sdamsdam/widget/R.java
@@ -8,188 +8,28 @@
 package com.sdamsdam.widget;
 
 public final class R {
-    public static final class array {
-        public static final int shouts=0x7f050000;
-    }
     public static final class attr {
     }
-    public static final class dimen {
-        public static final int gridview_height=0x7f060003;
-        public static final int gridview_width=0x7f060002;
-        public static final int init_height=0x7f060001;
-        /**  Default screen margins, per the Android Design guidelines. 
-         */
-        public static final int init_width=0x7f060000;
-        public static final int item_height=0x7f060005;
-        public static final int item_width=0x7f060004;
-    }
     public static final class drawable {
-        public static final int egg=0x7f020000;
-        public static final int egg_01=0x7f020001;
-        public static final int egg_02=0x7f020002;
-        public static final int egg_03=0x7f020003;
-        public static final int egg_04=0x7f020004;
-        public static final int egg_05=0x7f020005;
-        public static final int egg_06=0x7f020006;
-        public static final int egg_07=0x7f020007;
-        public static final int egg_08=0x7f020008;
-        public static final int egg_09=0x7f020009;
-        public static final int egg_10=0x7f02000a;
-        public static final int egg_100=0x7f02000b;
-        public static final int egg_101=0x7f02000c;
-        public static final int egg_102=0x7f02000d;
-        public static final int egg_103=0x7f02000e;
-        public static final int egg_104=0x7f02000f;
-        public static final int egg_105=0x7f020010;
-        public static final int egg_106=0x7f020011;
-        public static final int egg_107=0x7f020012;
-        public static final int egg_108=0x7f020013;
-        public static final int egg_109=0x7f020014;
-        public static final int egg_11=0x7f020015;
-        public static final int egg_110=0x7f020016;
-        public static final int egg_111=0x7f020017;
-        public static final int egg_112=0x7f020018;
-        public static final int egg_113=0x7f020019;
-        public static final int egg_114=0x7f02001a;
-        public static final int egg_115=0x7f02001b;
-        public static final int egg_116=0x7f02001c;
-        public static final int egg_117=0x7f02001d;
-        public static final int egg_118=0x7f02001e;
-        public static final int egg_119=0x7f02001f;
-        public static final int egg_12=0x7f020020;
-        public static final int egg_120=0x7f020021;
-        public static final int egg_121=0x7f020022;
-        public static final int egg_122=0x7f020023;
-        public static final int egg_123=0x7f020024;
-        public static final int egg_13=0x7f020025;
-        public static final int egg_14=0x7f020026;
-        public static final int egg_15=0x7f020027;
-        public static final int egg_16=0x7f020028;
-        public static final int egg_17=0x7f020029;
-        public static final int egg_18=0x7f02002a;
-        public static final int egg_19=0x7f02002b;
-        public static final int egg_20=0x7f02002c;
-        public static final int egg_21=0x7f02002d;
-        public static final int egg_22=0x7f02002e;
-        public static final int egg_23=0x7f02002f;
-        public static final int egg_24=0x7f020030;
-        public static final int egg_25=0x7f020031;
-        public static final int egg_26=0x7f020032;
-        public static final int egg_27=0x7f020033;
-        public static final int egg_28=0x7f020034;
-        public static final int egg_29=0x7f020035;
-        public static final int egg_30=0x7f020036;
-        public static final int egg_31=0x7f020037;
-        public static final int egg_32=0x7f020038;
-        public static final int egg_33=0x7f020039;
-        public static final int egg_34=0x7f02003a;
-        public static final int egg_35=0x7f02003b;
-        public static final int egg_36=0x7f02003c;
-        public static final int egg_37=0x7f02003d;
-        public static final int egg_38=0x7f02003e;
-        public static final int egg_39=0x7f02003f;
-        public static final int egg_40=0x7f020040;
-        public static final int egg_41=0x7f020041;
-        public static final int egg_42=0x7f020042;
-        public static final int egg_43=0x7f020043;
-        public static final int egg_44=0x7f020044;
-        public static final int egg_45=0x7f020045;
-        public static final int egg_46=0x7f020046;
-        public static final int egg_47=0x7f020047;
-        public static final int egg_48=0x7f020048;
-        public static final int egg_49=0x7f020049;
-        public static final int egg_50=0x7f02004a;
-        public static final int egg_51=0x7f02004b;
-        public static final int egg_52=0x7f02004c;
-        public static final int egg_53=0x7f02004d;
-        public static final int egg_54=0x7f02004e;
-        public static final int egg_55=0x7f02004f;
-        public static final int egg_56=0x7f020050;
-        public static final int egg_57=0x7f020051;
-        public static final int egg_58=0x7f020052;
-        public static final int egg_59=0x7f020053;
-        public static final int egg_60=0x7f020054;
-        public static final int egg_61=0x7f020055;
-        public static final int egg_62=0x7f020056;
-        public static final int egg_63=0x7f020057;
-        public static final int egg_64=0x7f020058;
-        public static final int egg_65=0x7f020059;
-        public static final int egg_66=0x7f02005a;
-        public static final int egg_67=0x7f02005b;
-        public static final int egg_68=0x7f02005c;
-        public static final int egg_69=0x7f02005d;
-        public static final int egg_70=0x7f02005e;
-        public static final int egg_71=0x7f02005f;
-        public static final int egg_72=0x7f020060;
-        public static final int egg_73=0x7f020061;
-        public static final int egg_74=0x7f020062;
-        public static final int egg_75=0x7f020063;
-        public static final int egg_76=0x7f020064;
-        public static final int egg_77=0x7f020065;
-        public static final int egg_78=0x7f020066;
-        public static final int egg_79=0x7f020067;
-        public static final int egg_80=0x7f020068;
-        public static final int egg_81=0x7f020069;
-        public static final int egg_82=0x7f02006a;
-        public static final int egg_83=0x7f02006b;
-        public static final int egg_84=0x7f02006c;
-        public static final int egg_85=0x7f02006d;
-        public static final int egg_86=0x7f02006e;
-        public static final int egg_87=0x7f02006f;
-        public static final int egg_88=0x7f020070;
-        public static final int egg_89=0x7f020071;
-        public static final int egg_90=0x7f020072;
-        public static final int egg_91=0x7f020073;
-        public static final int egg_92=0x7f020074;
-        public static final int egg_93=0x7f020075;
-        public static final int egg_94=0x7f020076;
-        public static final int egg_95=0x7f020077;
-        public static final int egg_96=0x7f020078;
-        public static final int egg_97=0x7f020079;
-        public static final int egg_98=0x7f02007a;
-        public static final int egg_99=0x7f02007b;
-        public static final int egg_all=0x7f02007c;
-        public static final int egg_detail=0x7f02007d;
-        public static final int home=0x7f02007e;
-        public static final int ic_launcher=0x7f02007f;
-        public static final int iphone=0x7f020080;
-        public static final int iphone_detail=0x7f020081;
-        public static final int iphone_expressiion=0x7f020082;
-        public static final int lg=0x7f020083;
-        public static final int lg_detail=0x7f020084;
-        public static final int lg_expression=0x7f020085;
-        public static final int motorola_detail=0x7f020086;
-        public static final int motorola_expression=0x7f020087;
-        public static final int pantech=0x7f020088;
-        public static final int pantech_detail=0x7f020089;
-        public static final int pantech_expression=0x7f02008a;
-        public static final int personal=0x7f02008b;
-        public static final int rammus=0x7f02008c;
-        public static final int rammus1=0x7f02008d;
-        public static final int rammus2=0x7f02008e;
-        public static final int rammus3=0x7f02008f;
-        public static final int rammus4=0x7f020090;
-        public static final int samsung=0x7f020091;
-        public static final int samsung_detail=0x7f020092;
-        public static final int samsung_expression=0x7f020093;
-        public static final int sony=0x7f020094;
-        public static final int sony_detail=0x7f020095;
-        public static final int sony_expression=0x7f020096;
+        public static final int chatbox=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int rammus=0x7f020002;
+        public static final int rammus1=0x7f020003;
+        public static final int rammus2=0x7f020004;
+        public static final int rammus3=0x7f020005;
+        public static final int rammus4=0x7f020006;
+        public static final int samsung=0x7f020007;
+        public static final int samsung_detail=0x7f020008;
     }
     public static final class id {
-        public static final int eggparts=0x7f090003;
-        public static final int empty_textview=0x7f090002;
-        public static final int frame=0x7f090000;
-        public static final int messages_gridview=0x7f090001;
+        public static final int imageView1=0x7f070000;
+        public static final int textView1=0x7f070001;
     }
     public static final class layout {
-        public static final int gridview_appwidget=0x7f030000;
-        public static final int gridview_item=0x7f030001;
-        public static final int mainlayout=0x7f030002;
+        public static final int widget_layout=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f070000;
-        public static final int numColumns=0x7f070001;
+        public static final int app_name=0x7f050000;
     }
     public static final class style {
         /** 
@@ -213,11 +53,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f060001;
     }
     public static final class xml {
         public static final int widget_configuration=0x7f040000;
