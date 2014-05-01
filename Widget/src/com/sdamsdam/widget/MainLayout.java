c607a597959b12be977c9a2b43a01018a2eeadf7
ruucm <bumbermin@naver.com>
2014-05-02 00:15:40 +0900

    1

---
+++ Widget/src/com/sdamsdam/widget/MainLayout.java
@@ -0,0 +1,39 @@
+package com.sdamsdam.widget;
+
+import android.app.*;
+import android.content.*;
+import android.os.*;
+import android.util.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainLayout extends Activity {
+	
+	
+	
+	
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainlayout);
+	}
+	
+	
+	public void mOnClick(View v){
+		
+    	switch(v.getId()){    	
+    		
+    	case R.id.setbutton:    		
+    		Intent intent = new Intent(this, Setting.class);
+    		startActivity(intent); 		
+    		break;
+    		
+    		
+    		
+    }	
+
+	
+}
+}
